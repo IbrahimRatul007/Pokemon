47a6157b9fa8ff82ae2a68c91720b7eedf9627ab
Ibrahim Ratul <ibrahim.ratul@brainstation-23.com>
2024-07-02 19:45:09 +0600

    use framer motion, make button component, make card component, update tailwind config file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Pokemon from "./components/pokemon";
+import Wrapper from "./components/UI/Wrapper";
+import Button from "./components/Button";
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const [limit, setLimit] = useState(12);
+  const [loading, setLoading] = useState(false);
+  function handleLimit() {
+    setLimit((prev) => prev + 12);
+  }
+
+  function handleCollapse() {
+    setLimit(12);
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`)
+      .then((res) => {
+        setPokemons(res.data.results);
+        setLoading(false);
+      });
+  }, [limit]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      {loading && <p>loading...</p>}
+      <ul className="flex gap-2 flex-wrap">
+        {pokemons.map((pokemon) => (
+          <Pokemon key={pokemon.name} name={pokemon.name} />
+        ))}
+      </ul>
+      <div className="flex gap-2 justify-center my-10 font-bold">
+        <Button onHandleFunction={handleLimit} text={"Next"} />
+        <Button onHandleFunction={handleCollapse} text={"Collapse"} />
+      </div>
+    </Wrapper>
   );
 }
 
---
+++ src/components/pokemon.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Pokemon = ({ name }) => {
+  return (
+    <motion.div
+      className="border-2 flex flex-col items-center gap-4 rounded-lg shadow-md p-4 flex-1 min-w-[350px] cursor-pointer"
+      whileHover={{ scale: 1.1, background: "grey" }}
+      whileTap={{ rotateY: 360 }}
+      transition={{ type: "tween", stiffness: 500 }}
+    >
+      <li className="text-xl font-bold">{name.toUpperCase()}</li>
+      <img src={`https://img.pokemondb.net/artwork/${name}.jpg`} />
+    </motion.div>
+  );
+};
+
+export default Pokemon;
